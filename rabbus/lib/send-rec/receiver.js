a5b8b29c1b45e24a6e935d8090138e2810bf10a6
Derick Bailey <derickbailey@gmail.com>
2015-05-05 07:23:57 -0500

    receiver now using middleware

--- rabbus/lib/send-rec/receiver.js
+++ rabbus/lib/send-rec/receiver.js
@@ -3,18 +3,17 @@ var util = require("util");
 var when = require("when");
 var _ = require("underscore");
 
+var Consumer = require("../consumer");
 var defaults = require("./defaults");
-var optionParser = require("../optionParser");
 
 // Receiver
 // --------
 
 function Receiver(rabbit, options){
-  this.rabbit = rabbit;
-  this.options = optionParser.parse(options, defaults);
+  Consumer.call(this, rabbit, options, defaults);
 }
 
-util.inherits(Receiver, Events.EventEmitter);
+util.inherits(Receiver, Consumer);
 
 // Instance Methods
 // ----------------
@@ -61,32 +60,34 @@ Receiver.prototype.receive = function(cb){
   var rabbit = this.rabbit;
   var queueOptions = this.options.queue;
   var messageType = this.options.messageType;
+  var middleware = this.middleware;
 
   this._start().then(function(){
 
     console.log("receiving from", queueOptions.name, messageType);
     that.emit("ready");
-    that.handler = rabbit.handle(messageType, function(msg){
 
-      function rejectMessage(){
-        that.emit("nack");
-        msg.nack();
-      }
+    var handler = middleware.prepare(function(config){
+      config.on("ack", that.emit.bind(that, "ack"));
+      config.on("nack", that.emit.bind(that, "nack"));
+      config.on("reject", that.emit.bind(that, "reject"));
 
-      function done(){
-        msg.ack();
-        that.emit("ack");
-      }
+      config.last(function(msg, properties, actions){
+        function done(){
+          actions.ack();
+        }
 
-      try {
-        cb(msg.body, done);
-      } catch(ex) {
-        rejectMessage();
-        that.emitError(ex);
-      }
+        try {
+          cb(msg, done);
+        } catch(ex) {
+          actions.nack();
+          that.emitError(ex);
+        }
+      });
 
     });
 
+    rabbit.handle(messageType, handler);
     rabbit.startSubscription(queueOptions.name);
 
   }).then(null, function(err){
